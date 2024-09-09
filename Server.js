68e262c57e056eef40ff6d0e1c9315b2bc0b5247
Angel B <angelb@Angels-MacBook-Pro.local>
2024-09-08 23:46:09 -0700

    server js updates

--- Server.js
+++ Server.js
@@ -1,13 +1,87 @@
 const express = require("express");
+const inquirer = require("inquirer");
+const {userInput} = require('pg');
+
 const app = express();
-
-
 const PORT = 3001;
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const {userInput}= new userInput({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'password',
+    port: 5050, //??
+});
+
+userInput.connect();
+
+const run = async () => {
+    const {schema} = await inquirer.prompt([
+        {
+        type: "list",
+        name: "schema",
+        message: "where would you like to be directed?",
+        choices: ["EMPLOYEE", "ROLE", "DEPARTMENT"],
+        },
+    ]);
+
+    const res = await userInput.query(`SELECT * FROM ${schema}`);
+    console.table(res.rows);
+
+    userInput.end();
+};
+
+
+const mainMenu = async () => {
+    const answer = await inquirer.prompt({
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: [
+            "View all employees",
+            "View all roles",
+            "View all departments",
+            "Add an employee",
+            "Add a role",
+            "Add a department",
+            "Update an employee role",
+            "Quit"
+        ]
+    });
+
+    switch (answer.option) {
+        case "View all employees":
+            viewEmployees();
+            break;
+        case "View all roles":
+            viewRoles();
+            break;
+        case "View all departments":
+            viewDepartments();
+            break;
+        case "Add an employee":
+            addEmployee();
+            break;
+        case "Add a role":
+            addRole();
+            break;
+        case "Add a department":
+            addDepartment();
+            break;
+        case "Update an employee role":
+            updateEmployeeRole();
+            break;
+        case "Quit":
+            quit();
+            break;
+    }
+};
+
+mainMenu();
 
 
 
