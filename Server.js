bbc4f4f3ab31fc7696f2d591bf44aac11576e370
Angel B <angelb@Angels-MacBook-Pro.local>
2024-09-08 23:04:52 -0700

    git ignore and schema

--- Server.js
+++ Server.js
@@ -1,2 +1,14 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql');
+const express = require("express");
+const app = express();
+
+
+const PORT = 3001;
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.listen(PORT, () => {console.log(`API server now on port http://localhost:PORT`);});
